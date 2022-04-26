7b42ff5479382ce8e2a74ffcedcd5af2509a9583
Rodrigo de Jesus Ramirez Zapata <roid85@gmail.comx>
2022-04-26 17:37:53 -0500

    Fix route

--- prod/main.tf
+++ prod/main.tf
@@ -49,7 +49,7 @@ resource "aws_default_route_table" "route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw
+    gateway_id = aws_internet_gateway.gw.id
   }
 }
 
