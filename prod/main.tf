cbf19267abc87e784266b587f4f4e1f783730824
Rodrigo de Jesus Ramirez Zapata <roid85@gmail.comx>
2022-06-22 13:22:08 -0500

    example book fix

--- prod/main.tf
+++ prod/main.tf
@@ -9,16 +9,16 @@ variable "server_port" {
 
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
-  instace_type = "t2.micro"
- vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
-
- tags {
-   name = "terraform-example"
+ 
+  tags = {
+    name = "terraform-example"
  } 
 }
 
