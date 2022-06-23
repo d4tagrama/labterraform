e7341ca690f488ced9468a82f2a4630c0c1af30c
Rodrigo de Jesus Ramirez Zapata <roid85@gmail.comx>
2022-06-23 10:09:40 -0500

    ASG

--- prod/main.tf
+++ prod/main.tf
@@ -10,6 +10,20 @@ variable "server_port" {
 output "public_ip" {
   value = "${aws_instance.example.public_ip}"
 }
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-40d28157"
+  instance_type = "t2.micro"
+  security_groups = [ "${aws_security_group.instance.id}" ]
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p "${var.server_port}" &
+    EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
@@ -32,5 +46,21 @@ resource "aws_security_group" "instance" {
     to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  min_size = 1
+  max_size = 2
+  availability_zones = [ "${data.aws_launch_configuration.example.id}}" ]
+
+  tag  {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
